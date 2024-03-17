4462cc086ed18417dab5c9aad4af736e64a420e1
Eugene Ilin <jenyay.ilin@gmail.com>
2024-03-17 18:57:54 +0300

    Добавлены примеры про работу со строками

---
+++ python language/src/09. Strings/multiline.py
@@ -0,0 +1,16 @@
+# Отступы и многострочный текст
+
+if True:
+    foo = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+
+    bar = """Lorem ipsum dolor sit amet,
+    consectetur adipiscing elit,
+    sed do eiusmod tempor incididunt
+    ut labore et dolore magna aliqua."""
+
+    print("foo:", foo)
+    print("----")
+    print("bar:", bar)
