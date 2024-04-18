6a3e877eacf8493fd7b7ea322db5712c800a76d8
Eugene Ilin <jenyay.ilin@gmail.com>
2024-04-18 18:51:11 +0300

    Обновление скрипта для демонстрации численной дисперсии

--- fdtd/src/python/fdtd_dispersion_vacuum.py
+++ fdtd/src/python/fdtd_dispersion_vacuum.py
@@ -13,29 +13,6 @@ from numpy.fft import fft
 import tools
 
 
-class GaussianPlaneWave:
-    ''' Класс с уравнением плоской волны для гауссова сигнала в дискретном виде
-    d - определяет задержку сигнала.
-    w - определяет ширину сигнала.
-    Sc - число Куранта.
-    eps - относительная диэлектрическая проницаемость среды, в которой расположен источник.
-    mu - относительная магнитная проницаемость среды, в которой расположен источник.
-    '''
-    def __init__(self, d, w, Sc=1.0, eps=1.0, mu=1.0):
-        self.d = d
-        self.w = w
-        self.Sc = Sc
-        self.eps = eps
-        self.mu = mu
-
-    def getE(self, m, q):
-        '''
-        Расчет поля E в дискретной точке пространства m
-        в дискретный момент времени q
-        '''
-        return np.exp(-(((q - m * np.sqrt(self.eps * self.mu) / self.Sc) - self.d) / self.w) ** 2)
-
-
 if __name__ == '__main__':
     # Волновое сопротивление свободного пространства
     W0 = 120.0 * np.pi
@@ -74,7 +51,6 @@ if __name__ == '__main__':
 
     Ez = np.zeros(maxSize)
     Hy = np.zeros(maxSize - 1)
-    source = GaussianPlaneWave(30.0, 3.0, Sc)
 
     # Коэффициенты для расчета ABC второй степени
     # Sc' для левой границы
@@ -128,17 +104,10 @@ if __name__ == '__main__':
         # Расчет компоненты поля H
         Hy = Hy + (Ez[1:] - Ez[:-1]) * Sc / (W0 * mu)
 
-        # Источник возбуждения с использованием метода
-        # Total Field / Scattered Field
-        Hy[sourcePos - 1] -= (Sc / (W0 * mu[sourcePos - 1])) * source.getE(0, q)
-
         # Расчет компоненты поля E
         Ez[1:-1] = Ez[1: -1] + (Hy[1:] - Hy[: -1]) * Sc * W0 / eps[1: -1]
 
-        # Источник возбуждения с использованием метода
-        # Total Field / Scattered Field
-        Ez[sourcePos] += ((Sc / (np.sqrt(eps[sourcePos] * mu[sourcePos]))) *
-                          source.getE(-0.5, q + 0.5))
+        Ez[sourcePos] += np.exp(-(q - 0.5 - 30.0) ** 2 / 9.0)
 
         # Граничные условия ABC второй степени (слева)
         Ez[0] = (k1Left * (k2Left * (Ez[2] + oldEzLeft2[0]) +
