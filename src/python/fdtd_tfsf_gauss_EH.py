44207d54fddf6ec0042215f9617eb682d332e575
Eugene Ilin <jenyay.ilin@gmail.com>
2025-10-21 18:56:46 +0300

    Добавлен пример для демонстрации полей E и H при использовании метода TFSF

---
+++ src/python/fdtd_tfsf_gauss_EH.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+'''
+Метод полного поля / рассеянного поля (TF / SF).
+Гауссов импульс.
+'''
+
+import numpy
+
+import tools
+
+
+class GaussianPlaneWave:
+    ''' Класс с уравнением плоской волны для гауссова сигнала в дискретном виде
+    d - определяет задержку сигнала.
+    w - определяет ширину сигнала.
+    Sc - число Куранта.
+    eps - относительная диэлектрическая проницаемость среды, в которой расположен источник.
+    mu - относительная магнитная проницаемость среды, в которой расположен источник.
+    '''
+
+    def __init__(self, d, w, Sc=1.0, eps=1.0, mu=1.0):
+        self.d = d
+        self.w = w
+        self.Sc = Sc
+        self.eps = eps
+        self.mu = mu
+
+    def getE(self, m, q):
+        '''
+        Расчет поля E в дискретной точке пространства m
+        в дискретный момент времени q
+        '''
+        return numpy.exp(-(((q - m * numpy.sqrt(self.eps * self.mu) / self.Sc) - self.d) / self.w) ** 2)
+
+
+if __name__ == '__main__':
+    # Характеристическое сопротивление свободного пространства
+    Z0 = 120.0 * numpy.pi
+
+    # Число Куранта
+    Sc = 1.0
+
+    # Время расчета в отсчетах
+    maxTime = 400
+
+    # Размер области моделирования в отсчетах
+    maxSize = 200
+
+    # Положение источника в отсчетах
+    sourcePos = 50
+
+    # Датчики для регистрации поля
+    probesPos = [75]
+    probes = [tools.Probe(pos, maxTime) for pos in probesPos]
+
+    Ez = numpy.zeros(maxSize)
+    Hy = numpy.zeros(maxSize)
+
+    source = GaussianPlaneWave(30.0, 10.0, Sc)
+
+    for probe in probes:
+        probe.addData(Ez, Hy)
+
+    # Параметры отображения поля E
+    display_ymin = -1.1
+    display_ymax = 1.1
+
+    # Создание экземпляра класса для отображения
+    # распределения поля в пространстве
+    display = tools.AnimateFieldDisplayEH(maxSize,
+                                          display_ymin, display_ymax)
+
+    display.activate()
+    display.drawProbes(probesPos)
+    display.drawSources([sourcePos])
+
+    for q in range(1, maxTime):
+        # Граничные условия для поля H
+        Hy[-1] = Hy[-2]
+
+        # Расчет компоненты поля H
+        Hy[:-1] = Hy[:-1] + (Ez[1:] - Ez[:-1]) * Sc / Z0
+
+        # Источник возбуждения с использованием метода
+        # Total Field / Scattered Field
+        Hy[sourcePos - 1] -= (Sc / Z0) * source.getE(sourcePos, q)
+        # Hy[sourcePos - 1] -= (Sc / Z0) * source.getE(0, q)
+
+        # Граничные условия для поля E
+        Ez[0] = Ez[1]
+
+        # Расчет компоненты поля E
+        Ez[1:] = Ez[1:] + (Hy[1:] - Hy[:-1]) * Sc * Z0
+
+        # Источник возбуждения с использованием метода
+        # Total Field / Scattered Field
+        Ez[sourcePos] += Sc * source.getE(sourcePos - 0.5, q + 0.5)
+        # Ez[sourcePos] += Sc * source.getE(-0.5, q + 0.5)
+
+        # Регистрация поля в датчиках
+        for probe in probes:
+            probe.addData(Ez, Hy)
+
+        if q % 2 == 0:
+            display.updateData(Ez, Hy, q)
+
+    display.stop()
+
+    # Отображение сигналов, сохраненных в датчиках
+    tools.showProbeSignals(probes, -1.1, 1.1)
